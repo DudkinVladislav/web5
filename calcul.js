2d4b933f7ecd2df4f52ca69451800f57a4942a13
DudkinVladislav <90377440+DudkinVladislav@users.noreply.github.com>
2021-10-10 12:58:41 +0300

    Update calcul.js

--- calcul.js
+++ calcul.js
@@ -1,10 +1,10 @@
-function proizvedenie()
-{
-const chek =/^-+\d+$|\d+$/
-let num1 = document.getElementsByName("number1");
-let num2 = document.getElementsByName("number2");
-if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
-    document.getElementById('out').innerHTML='Введённые данные неверны.';
+function proizvedenie() {
+    const chek =/^-+\d+$|\d+$/
+    let num1 = document.getElementsByName("number1");
+    let num2 = document.getElementsByName("number2");
+	
+    if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
+        document.getElementById('out').innerHTML='Введённые данные неверны.';
     }
     else{
         let num3 = document.getElementById("out");
@@ -12,11 +12,12 @@ if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
         num2[0] = Number.parseInt(num2[0]);
         num3.innerHTML = num1[0].value * num2[0].value;
 	return false;   
-	}
+    }
 }
-	window.addEventListener('DOMContentLoaded',function(event)
-		{
-	  console.log("DOM finaly loaded and parsed");
-	  let knopka = document.getElementById("my-button");
-  		knopka.addEventListener("click", proizvedenie);
-		})
+
+window.addEventListener('DOMContentLoaded',function(event) {
+    console.log("DOM finaly loaded and parsed");
+	
+    let knopka = document.getElementById("my-button");
+    knopka.addEventListener("click", proizvedenie());
+})
