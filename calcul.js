aa5ec8f8d72922922300110967369a3222e612f6
DudkinVladislav <90377440+DudkinVladislav@users.noreply.github.com>
2021-10-10 12:25:11 +0300

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,18 @@
+function proizvedenie(){
+const chek =/^-+\d+$|\d+$/
+let num1 = document.getElementsByName("number1");
+let num2 = document.getElementsByName("number2");
+if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
+    document.getElementById('out').innerHTML='¬ведЄнные данные неверны.';
+    }
+    else{
+        let num3 = document.getElementById("out");
+        num1[0] = Number.parseInt(num1[0]);
+        num2[0] = Number.parseInt(num2[0]);
+        num3.innerHTML = num1[0].value * num2[0].value;
+	return false;   
+	}
+	window.addEventListener('DOMContentLoaded',function(event){
+	  console.log("DOM finaly loaded and parsed");
+	  let knopka = document.getElementById("my-button");
+  		knopka.addEventListener("click", proizvedenie);
